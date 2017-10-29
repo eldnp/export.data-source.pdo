6674da6d5041e541f0d835e7946683775e1fb5b9
Oleg Verevskoy <verevskoy@gmail.com>
2017-10-29 14:34:08 +0300

    PdoStatementDataSource and PdoStatementDataSourceTest

---
+++ lib/PdoStatementDataSource.php
@@ -0,0 +1,126 @@
+<?php
+/*
+ * This file is part of Eldnp/export.data-source.pdo.
+ *
+ * Eldnp/export.data-source.pdo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Eldnp/export.data-source.pdo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Eldnp/export.data-source.pdo.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @see       https://github.com/eldnp/export.data-source.pdo for the canonical source repository
+ * @copyright Copyright (c) 2017 Oleg Verevskoy <verevskoy@gmail.com>
+ * @license   https://github.com/eldnp/export.data-source.pdo/blob/master/LICENSE GNU GENERAL PUBLIC LICENSE Version 3
+ */
+
+namespace Eldnp\Export\DataSource\Pdo;
+
+use Eldnp\Export\DataSource\Pdo\Exception\LogicException;
+use Eldnp\Export\Map\AbstractMapDataSource;
+
+/**
+ * Class PdoStatementDataSource
+ *
+ * @package Eldnp\Export\DataSource\Pdo
+ */
+class PdoStatementDataSource extends AbstractMapDataSource
+{
+    /**
+     * @var \PDOStatement
+     */
+    private $statement;
+
+    /**
+     * @see constants \PDO::FETCH_...
+     *
+     * @var int|null
+     */
+    private $fetchStyle;
+
+    /**
+     * @var array|false
+     */
+    private $rawData;
+
+    /**
+     * @var int
+     */
+    private $counter = 0;
+
+    /**
+     * @var bool
+     */
+    private $allowRewind = true;
+
+    /**
+     * PdoStatementDataSource constructor.
+     *
+     * @param \PDOStatement $statement
+     * @param int|null $fetchStyle
+     */
+    public function __construct(\PDOStatement $statement, $fetchStyle = null)
+    {
+        $this->statement = $statement;
+        $this->fetchStyle = $fetchStyle;
+    }
+
+    private function fetchNext()
+    {
+        $this->rawData = $this->statement->fetch($this->fetchStyle);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function currentMap()
+    {
+        return $this->rawData;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function next()
+    {
+        $this->fetchNext();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function key()
+    {
+        return $this->counter;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function valid()
+    {
+        if ($valid = false !== $this->rawData) {
+            $this->counter++;
+        }
+        return  $valid;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function rewind()
+    {
+        if (!$this->allowRewind) {
+            throw new LogicException('rewind not supported');
+        }
+
+        $this->allowRewind = false;
+        $this->fetchNext();
+    }
+}
